80b121c924aaf966e2697004af8b82973b0083b5
Gordon Yin <gyin1@uci.edu>
2018-10-17 16:22:53 -0700

    New Updates

--- Wumpus_World_Python_Shell/src/MyAI.py
+++ Wumpus_World_Python_Shell/src/MyAI.py
@@ -25,8 +25,15 @@ class MyAI ( Agent ):
         # ======================================================================
         # YOUR CODE BEGINS
         # ======================================================================
-        
-        pass
+        self.current = (1, 1)
+        self.last_visited = None
+        self.orientation = "right"
+        self.visited = dict()
+        self.visited[self.current] = 1
+        self.safe = dict()
+        self.danger = dict()
+        self.xlim = 100000
+        self.ylim = 100000
         # ======================================================================
         # YOUR CODE ENDS
         # ======================================================================
@@ -35,8 +42,20 @@ class MyAI ( Agent ):
         # ======================================================================
         # YOUR CODE BEGINS
         # ======================================================================
-        
-        return Agent.Action.CLIMB
+        print("Current: ", self.current)
+        print("Just Visited: ", self.last_visited)
+        print(self.visited)
+        self.visited[self.last_visited] = 1
+        if self.current == (1, 1):
+            coord = (self.current[0] + 1, self.current[1])
+            return self.moveTo(coord)
+        if bump:
+            if self.orientation == "right":
+                self.xlim = self.current[0]
+            elif self.orientation == "up":
+                self.ylim = self.current[1]
+        if breeze:
+            pass
         # ======================================================================
         # YOUR CODE ENDS
         # ======================================================================
@@ -44,7 +63,75 @@ class MyAI ( Agent ):
     # ======================================================================
     # YOUR CODE BEGINS
     # ======================================================================
+    def moveTo(self, coord):
+        if coord[0] == 1 + self.current[0]:
+            return self.move_right(coord)
+        elif coord[1] == 1 + self.current[1]:
+            return self.move_up(coord)
+        elif coord[0] == self.current[0] - 1:
+            return self.move_left(coord)
+        elif coord[1] == self.current[1] - 1:
+            return self.move_down(coord)
 
+    def move_right(self, coord):
+        if self.orientation == "right":
+            self.last_visited = self.current
+            self.current = coord
+            return Agent.Action.FORWARD
+        elif self.orientation == "up":
+            self.orientation = "right"
+            return Agent.Action.TURN_RIGHT
+        elif self.orientation == "down":
+            self.orientation = "right"
+            return Agent.Action.TURN_LEFT
+        elif self.orientation == "left":
+            self.orientation = "down"
+            return Agent.Action.TURN_LEFT
+
+    def move_up(self, coord):
+        if self.orientation == "up":
+            self.last_visited = self.current
+            self.current = coord
+            return Agent.Action.FORWARD
+        elif self.orientation == "left":
+            self.orientation = "up"
+            return Agent.Action.TURN_RIGHT
+        elif self.orientation == "right":
+            self.orientation = "up"
+            return Agent.Action.TURN_LEFT
+        elif self.orientation == "down":
+            self.orientation = "right"
+            return Agent.Action.TURN_LEFT
+
+    def move_left(self, coord):
+        if self.orientation == "left":
+            self.last_visited = self.current
+            self.current = coord
+            return Agent.Action.FORWARD
+        elif self.orientation == "up":
+            self.orientation = "left"
+            return Agent.Action.TURN_LEFT
+        elif self.orientation == "down":
+            self.orientation = "left"
+            return Agent.Action.TURN_RIGHT
+        elif self.orientation == "right":
+            self.orientation = "up"
+            return Agent.Action.TURN_LEFT
+
+    def move_down(self, coord):
+        if self.orientation == "down":
+            self.last_visited = self.current
+            self.current = coord
+            return Agent.Action.FORWARD
+        elif self.orientation == "right":
+            self.orientation = "down"
+            return Agent.Action.TURN_RIGHT
+        elif self.orientation == "left":
+            self.orientation = "down"
+            return Agent.Action.TURN_LEFT
+        elif self.orientation == "up":
+            self.orientation = "left"
+            return Agent.Action.TURN_LEFT
     
     # ======================================================================
     # YOUR CODE ENDS
